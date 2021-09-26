10c5c985b719221173c6b5e4c97be982541bddb6
elyor0701 <elyorbekuralov@gmail.com>
2021-09-26 13:51:15 +0500

    3/D is done

--- B/3/D.cpp
+++ B/3/D.cpp
@@ -2,45 +2,47 @@
 #include <set>
 #include <sstream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
 void yes_set (set<int>& main, set<int>& second) {
-	for (auto it = main.begin(); it != main.end(); ) {
-		if (second.count(*it) == 0)
-			it = main.erase(it);
-		++it;
+	for (auto it = second.begin(); it != second.end(); ) {
+		if (main.count(*it) == 0)
+			it = second.erase(it);
+		else 
+			++it;
 	}
+	main = second;
 }
 
 void no_set (set<int>& main, set<int>& second) {
-	for (auto it = main.begin(); it != main.end(); ) {
-		if (second.count(*it) == 1)
-			it = main.erase(it);
+	for (auto it = second.begin(); it != second.end(); ) {
+		if (main.count(*it) == 1)
+			main.erase(*it);
 		++it;
 	}
 }
 
 void help_cout (set<int>& main) {
-	for (auto item : main)
+	for (auto& item : main)
 		cout << item << " ";
 }
 
 int main () {
 	int n;
 	cin >> n;
+	cin.ignore(32767, '\n');
 	set<int> main_numbers;
 	
 	for (int i = 1; i <= n; i++)
 		main_numbers.insert(i);
-	cout << "iam here1" << endl;
 	string s;
 	while (getline(cin, s)) {
 		if (s == "HELP") {
-			cout << "iam here2" << endl;
 			help_cout(main_numbers);
+			break;
 		} else {
-			cout << "iam here3" << endl;
 			stringstream ss(s);
 			int number;
 			set<int> predicted_numbers;
@@ -49,6 +51,7 @@ int main () {
 			}
 			string answer;
 			cin >> answer;
+			cin.ignore(32767, '\n');
 			
 			if (answer == "YES") {
 				yes_set(main_numbers, predicted_numbers);
